31e503aee5750ded0d55b8c69ab6e4a08891254e
Pu Shi <mr.pu.shi@gmail.com>
2020-11-04 13:54:43 +0800

    Vote for proposals.

--- farm_helper.js
+++ farm_helper.js
@@ -10,6 +10,23 @@
 #userToken
 [user] => token
 
+#proposal
+[proposalId] => ["中文", "en"]
+
+#proposalVoters
+[proposalId] => []
+
+#proposalAction
+[proposalId + ":" + user] => 1 / -1
+
+#proposalStat
+[proposalId] => {
+  approval: 0,
+  disapproval: 0,
+  expiration: time
+}
+
+
 */
 
 const UNIVERSAL_PRECISION = 12;
@@ -19,12 +36,12 @@ const TOKEN_WHITE_LIST = [
 ['lol', 'xlp1603126828720', '0'],
 ['metx', 'xlp1603713983453', '0'],
 ['otbc', 'xlp1603292350826', '0'],
-['xusd', 'xusd', '1'],
+['xusd', 'xusd', '2'],
 ['zs', 'xlp1603126720057', '1'],
 ['ppt', 'xlp1603126734507', '2'],
-['tpt', 'xlp1603126793694', '2'],
+['tpt', 'xlp1603126793694', '1'],
 ['iost', 'xlp1603017606495', '4'],
-['vost', 'xlp1603126749670', '4'],
+['vost', 'xlp1603126749670', '3'],
 ['xg', 'xlp1603126781817', '25']
 ];
 const ROUND_DOWN = 1;
@@ -136,6 +153,113 @@ class FarmHelper {
   _minusTotalVote(amountStr) {
     const currentStr = this._getTotalVote();
     this._setTotalVote(new BigNumber(currentStr).minus(amountStr).toFixed(UNIVERSAL_PRECISION));
+  }
+
+  _setUserAction(proposalId, who, action) {
+    const key = proposalId + ":" + who;
+    storage.mapPut("proposalAction", key, action.toString());
+  }
+
+  _getUserAction(proposalId, who) {
+    const key = proposalId + ":" + who;
+    return +storage.mapGet("proposalAction", key) || 0;
+  }
+
+  _hasUserAction(proposalId, who) {
+    const key = proposalId + ":" + who;
+    return storage.mapHas("proposalAction", key);
+  }
+
+  addProposal(proposalId, textZh, textEn) {
+    this._requireOwner();
+
+    storage.mapPut("proposal", proposalId, JSON.stringify([textZh, textEn]));
+
+    const now = Math.floor(tx.time / 1e9);
+
+    storage.mapPut("proposalStat", proposalId, JSON.stringify({
+      approval: 0,
+      disapproval: 0,
+      expiration: now + 3600 * 24
+    }));
+
+    storage.mapPut("proposalVoters", proposalId, "[]");
+  }
+
+  changeProposal(proposalId, textZh, textEn) {
+    this._requireOwner();
+
+    storage.mapPut("proposal", proposalId, JSON.stringify([textZh, textEn]));
+  }
+
+  _getAllVoters(proposalId) {
+    return JSON.parse(storage.mapGet("proposalVoters", proposalId));
+  }
+
+  _addOneVoter(proposalId, who) {
+    const list = JSON.parse(storage.mapGet("proposalVoters", proposalId));
+    list.push(who);
+    storage.mapPut("proposalVoters", proposalId, JSON.stringify(list));
+  }
+
+  _getProposalStat(proposalId) {
+    return JSON.parse(storage.mapGet("proposalStat", proposalId));
+  }
+
+  _setProposalStat(proposalId, stat) {
+    storage.mapPut("proposalStat", proposalId, JSON.stringify(stat));
+  }
+
+  _actionOnProposal(proposalId, value) {
+    if (this._hasUserAction(tx.publisher)) {
+      throw "already voted";
+    }
+
+    const now = Math.floor(tx.time / 1e9);
+    const stat = this._getProposalStat(proposalId);
+    if (now > stat.expiration) {
+      throw "expired";
+    }
+
+    this._addOneVoter(proposalId, tx.publisher);
+    this._setUserAction(proposalId, tx.publisher, value);
+  }
+
+  approveProposal(proposalId) {
+    this._actionOnProposal(proposalId, "1");
+  }
+
+  disapproveProposal(proposalId) {
+    this._actionOnProposal(proposalId, "-1");
+  }
+
+  checkProposal(proposalId) {
+    const now = Math.floor(tx.time / 1e9);
+    const stat = this._getProposalStat(proposalId);
+    if (now > stat.expiration) {
+      throw "expired";
+    }
+
+    stat.approval = 0;
+    stat.disapproval = 0;
+
+    const list = this._getAllVoters(proposalId);
+    list.forEach(who => {
+      const action = this._getUserAction(proposalId, who);
+      const info = JSON.parse(blockchain.call(
+          this._getFarm(), "getUserTokenInfo", [who, this._getMain()])[0]);
+      if (info && info.amount) {
+        if (action * 1 > 0) {
+          stat.approval += info.amount * 1;
+        } else {
+          stat.disapproval += info.amount * 1;
+        }
+      }
+    });
+
+    stat.approval = +stat.approval.toFixed(UNIVERSAL_PRECISION);
+    stat.disapproval = +stat.disapproval.toFixed(UNIVERSAL_PRECISION);
+    this._setProposalStat(proposalId, stat);
   }
 
   updatePools() {
